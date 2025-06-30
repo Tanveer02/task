edaf3f0d7816d6e6c0175bd24a44ac3b3f682668
kinanadohadwala@gmail.com <kinanadohadwala@gmail.com>
2025-06-30 15:00:42 +0530

    chnage

--- App.tsx
+++ App.tsx
@@ -18,53 +18,45 @@ export default function App() {
   const peer = new Peer();
 
   return (
-    /* CONTINUE ON IT */
-    // <NavigationContainer>
-    //   <Stack.Navigator>
-    //     <Stack.Screen name="Offer">
-    //       {() => (
-    //         <>
-    //           {!offer ? (
-    //             <Button
-    //               title="Create Offer"
-    //               onPress={async () => setOffer(await peer.createOffer())}
-    //             />
-    //           ) : (
-    //             <OfferQRCode offer={offer} />
-    //           )}
-    //           {offer && (
-    //             <Stack.Screen name="ScanOffer">
-    //               {({navigation}) => (
-    //                 <QRScanner
-    //                   onScan={value => {
-    //                     if (value) {
-    //                       const offer = JSON.parse(value);
-    //                       setOffer(offer); // store in state
-    //                       navigation.navigate('Share'); // or next screen
-    //                     } else {
-    //                       Alert.alert('Scanned QR is invalid');
-    //                     }
-    //                   }}
-    //                   onClose={() => navigation.goBack()}
-    //                 />
-    //               )}
-    //             </Stack.Screen>
-    //           )}
-    //         </>
-    //       )}
-    //     </Stack.Screen>
-    //     <Stack.Screen name="Share" component={ShareScreen} />
-    //     <Stack.Screen name="Receive">
-    //       {() => answer && <ReceiveScreen answerQR={answer} />}
-    //     </Stack.Screen>
-    //   </Stack.Navigator>
-    // </NavigationContainer>
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#f0f0f0',
-      }}></View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Offer">
+          {() => (
+            <>
+              {!offer ? (
+                <Button
+                  title="Create Offer"
+                  onPress={async () => setOffer(await peer.createOffer())}
+                />
+              ) : (
+                <OfferQRCode offer={offer} />
+              )}
+              {offer && (
+                <Stack.Screen name="ScanOffer">
+                  {({navigation}) => (
+                    <QRScanner
+                      onScan={value => {
+                        if (value) {
+                          const offer = JSON.parse(value);
+                          setOffer(offer); // store in state
+                          navigation.navigate('Share'); // or next screen
+                        } else {
+                          Alert.alert('Scanned QR is invalid');
+                        }
+                      }}
+                      onClose={() => navigation.goBack()}
+                    />
+                  )}
+                </Stack.Screen>
+              )}
+            </>
+          )}
+        </Stack.Screen>
+        <Stack.Screen name="Share" component={ShareScreen} />
+        <Stack.Screen name="Receive">
+          {() => answer && <ReceiveScreen answerQR={answer} />}
+        </Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
