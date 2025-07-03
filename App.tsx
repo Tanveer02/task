9ad6d79dd4ec20ca3aaaf5b9a7c58f57e9c08385
kinanadohadwala@gmail.com <kinanadohadwala@gmail.com>
2025-07-03 17:01:54 +0530

    wip/image list animation(Apple iinvite animation)

--- App.tsx
+++ App.tsx
@@ -2,16 +2,16 @@ import React, {useState} from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import {Peer} from './src/webrtc/Peer';
 import {commonStyle} from './src/constants/commonStyle';
-import ImagesList from './animatedSrc/ImagesList/ImagesList';
 import 'react-native-gesture-handler';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {View} from 'react-native';
+import ImageSlider from './animatedSrc/ImageSlider/ImageSlider';
 
 export default function App() {
   return (
     <GestureHandlerRootView style={{flex: 1}}>
       <View style={commonStyle.container}>
-        <ImagesList />
+        <ImageSlider />
       </View>
     </GestureHandlerRootView>
   );
